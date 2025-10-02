67c399be3fe4916e45029d077bcabc65bd13aab1
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-10-01 23:43:17 -0400

    clase para poder convertir fecha

--- app/src/main/java/edu/ucne/liamell_cruz_p1_ap2/data/local/ClassConverter/ClassConverter.kt
+++ app/src/main/java/edu/ucne/liamell_cruz_p1_ap2/data/local/ClassConverter/ClassConverter.kt
@@ -1,15 +1,19 @@
 package edu.ucne.liamell_cruz_p1_ap2.data.local.ClassConverter
 import androidx.room.TypeConverter
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class Converters {
-    private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
 
     @TypeConverter
-    fun fromLocalDate(date: LocalDate): String = date.format(formatter)
+    fun fromLocalDate(date: LocalDate): String {
+        return date.format(formatter)
+    }
 
     @TypeConverter
-    fun toLocalDate(dateString: String): LocalDate = LocalDate.parse(dateString, formatter)
+    fun toLocalDate(dateString: String): LocalDate {
+        return LocalDate.parse(dateString, formatter)
+    }
 }
